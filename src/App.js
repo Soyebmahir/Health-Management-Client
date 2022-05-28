3da73987ea9a397576a7eab854f8ae24530dcb8d
Soyeb <tssoyeb@gmail.com>
2022-05-28 14:29:26 +0600

    test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Navbar from './Pages/Shared/Navbar';
 import { Routes, Route, Link } from "react-router-dom";
-import Home from './Pages/Home/Home';
+import Home from './Pages/Home/Home'; 
 import About from './Pages/About/About';
 import Login from './Pages/Login/Login';
 import Appointment from './Pages/Appointment/Appointment';
